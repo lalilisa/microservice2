f1ab416a75dcf546a84bbfc31fdb38541523fe1a
maivantri309@gmail.com <trimv@hiworld.com.vn>
2023-02-17 15:59:14 +0700

    fix fetching config

--- profilemanagement/src/main/java/com/javadevjournal/profilemanagement/ProfilemanagementApplication.java
+++ profilemanagement/src/main/java/com/javadevjournal/profilemanagement/ProfilemanagementApplication.java
@@ -2,6 +2,7 @@ package com.javadevjournal.profilemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
